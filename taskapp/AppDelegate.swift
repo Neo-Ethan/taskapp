2e7f57562271b47703307ce3cb23dc761813d311
Hiro Katoh <katoh@fuji-corpo.co.jp>
2023-03-22 14:21:24 +0900

    230322

--- taskapp/AppDelegate.swift
+++ taskapp/AppDelegate.swift
@@ -6,31 +6,44 @@
 //
 
 import UIKit
+import UserNotifications   // 追加　Chapter 7.2
 
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+@main                             // UNUserNotificationCenterDelegateを追加 Chapter 7.5
+class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDelegate {
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // ユーザーに通知の許可を求める
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            // enable or disable features based on authorization
+        } // --- ここまで追加 --- Chapter 7.2
+        center.delegate = self   // 追加　Chapter7.5  フォアグラウンドでも通知
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
+    
+    // アプリがフォアグラウンドの時に通知を受け取ると呼ばれるメソッド   --- ここから --- Chapter7.5
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .list, .sound])
+    } // --- ここまで追加 --- Capter7.5
+}
 
 
+
+// MARK: UISceneSession Lifecycle
+
+func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+}
+
+func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    // Called when the user discards a scene session.
+    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 }
 
--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -6,27 +6,106 @@
 //
 
 import UIKit
+import RealmSwift    // 追加完了　chapter6.10
+import UserNotifications   // 追加　Chapter 7.3
 
 class InputViewController: UIViewController {
-
-    @IBOutlet weak var DatePicker: UIDatePicker!
-    @IBOutlet weak var TextView: UITextView!
-    @IBOutlet weak var TextField: UITextField!
+    
+    @IBOutlet weak var category: UITextField!
+    
+    //Chapter 6.8
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    let realm = try! Realm()  //追加完了　chapter6.10
+    var task: Task!  // 追加完了  chapter6.8
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Chapter 6.10
+        // 背景をタップしたらdismissKeyboardメソッドを呼ぶように設定する
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(dismissKeyboard))
+        self.view.addGestureRecognizer(tapGesture)
+        
+        titleTextField.text = task.title       //タイトル内容をtextFielに反省する指示
+        contentsTextView.text = task.contents
+        datePicker.date = task.date
+        category.text = task.category
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        try! realm.write {
+            self.task.title = self.titleTextField.text!   //タイトル入力した内容を保存する指示
+            self.task.contents = self.contentsTextView.text //内容入力した内容を保存する指示
+            self.task.date = self.datePicker.date          //日付入力した内容を保存する指示
+            self.task.category = self.category.text!
+            self.realm.add(self.task, update: .modified)
+        }
+        
+        setNotification(task: task)   // 追加 Chapter 7.3
+        
+        super.viewWillDisappear(animated)
     }
-    */
-
+    
+    // タスクのローカル通知を登録する　--- ここから ---　追加 Chapter 7.3
+    func setNotification(task: Task){
+        let content = UNMutableNotificationContent()
+        // タイトルと内容を設定（中身が無い場合メッセージ無しで音だけの通知になるので「（××なし）」を表示する）
+        if task.title == "" {
+            content.title = "(タイトルなし)"
+        } else {
+            content.title = task.title
+        }
+        if task.contents == "" {
+            content.body = task.contents
+        } else {
+            content.body = task.contents
+        }
+        content.sound = UNNotificationSound.default
+        
+        // ローカル通知が発動する triger(日付マッチ)を作成
+        let calendar = Calendar.current
+        let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: task.date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+  
+        // identifier, content, triggerからローカル通知を作成(identifierが同じだとローカル通知を上書き保存）
+        let request = UNNotificationRequest(identifier: String(task.id), content: content, trigger: trigger)
+        
+        
+        // ローカル通知を登録
+        let center = UNUserNotificationCenter.current()
+        center.add(request) {(error) in
+            print(error ?? "ローカル通知登録 OK") // error が nil ならローカル通知の登録に成功したと表示します。error が存在すれば error を表示します。
+        }
+            
+        // 未通知のローカル通知一覧をログ出力
+        center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+            for request in requests {
+                print("/---------------")
+                print(request)
+                print("---------------/")
+            }
+        }
+    } // --- ここまで追加 --- Chapter 7.3
+    
+    @objc func dismissKeyboard(){
+        // キーボードを閉じる
+        view.endEditing(true)
+    }
 }
+
+// Do any additional setup after loading the view.
+
+
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
